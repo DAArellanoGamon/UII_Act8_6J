63e21ccf4b26f10f74b7d8e6316a71ab7a221cf3
DAVID_ALEJANDRO_ARELLANO_GAMON <a.21308051280429@cbtis128.edu.mx>
2024-05-15 19:48:19 +0000

    ac

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,141 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+/// Flutter code sample for [TabBar].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+void main() => runApp(const TabBarApp());
+
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const TabBarExample(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class TabBarExample extends StatefulWidget {
+  const TabBarExample({super.key});
+
+  @override
+  State<TabBarExample> createState() => _TabBarExampleState();
+}
+
+/// [AnimationController]s can be created with `vsync: this` because of
+/// [TickerProviderStateMixin].
+class _TabBarExampleState extends State<TabBarExample>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        backgroundColor: Color(0xffff0707),
+        title: const Text(
+          'Ferrateria Arellano 0429',
+          style: TextStyle(color: Colors.white),
         ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(
+                Icons.save,
+                color: Colors.white,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.fire_truck_outlined,
+                color: Colors.white,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              "assets/images/Taladro_BOSH.png",
+              width: 400,
+              height: 300,
+              fit: BoxFit.contain,
+              frameBuilder: (context, child, frame, loaded) {
+                if (loaded) {
+                  return child;
+                }
+                return AnimatedOpacity(
+                  opacity: frame == null ? 0 : 1,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeOut,
+                  child: child,
+                );
+              },
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              "assets/images/taladro_Milwakee.png",
+              width: 400,
+              height: 300,
+              fit: BoxFit.contain,
+              frameBuilder: (context, child, frame, loaded) {
+                if (loaded) {
+                  return child;
+                }
+                return AnimatedOpacity(
+                  opacity: frame == null ? 0 : 1,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeOut,
+                  child: child,
+                );
+              },
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              "assets/images/Taladro_Stanley.png",
+              width: 400,
+              height: 300,
+              fit: BoxFit.contain,
+              frameBuilder: (context, child, frame, loaded) {
+                if (loaded) {
+                  return child;
+                }
+                return AnimatedOpacity(
+                  opacity: frame == null ? 0 : 1,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeOut,
+                  child: child,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
